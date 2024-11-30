1863403d26c581fbd7ea47b112d9bda57e0d136d
NehaSharma3496 <sharmaneha3496@gmail.com>
2024-11-30 10:29:07 +0530

    Reorganize imports in SubAdminRoutes for consistency; move Dashboard import to the correct position

--- Frontend/src/Routes/SubAdminRoutes.jsx
+++ Frontend/src/Routes/SubAdminRoutes.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Sidebar from '../components/layouts/Sidebar';
 import Header from '../components/layouts/Header';
-import Dashboard from '../components/subAdmin/subadminDashboard/Dashboard';
 // import AddClient from '../components/subadmin/Client/AddClient';
 import AddClient from '../components/subAdmin/Client/AddClient';
 import AllClient from '../components/subAdmin/Client/Allclient';
+import Dashboard from '../components/subAdmin/subadminDashboard/Dashboard';
 
 const SubAdminRoutes = () => {
   return (

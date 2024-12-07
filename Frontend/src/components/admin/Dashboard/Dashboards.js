43ca5a000bdbb2e566fb8fd68255df4476c0f305
Chandra6388 <cppatel6388@gmail.com>
2024-12-07 10:35:31 +0530

    Update card title in Dashboards component from 'Two Days Live Account' to 'Three Days Live Account'

--- Frontend/src/components/admin/Dashboard/Dashboards.js
+++ Frontend/src/components/admin/Dashboard/Dashboards.js
@@ -354,7 +354,7 @@ const Dashboards = () => {
                                         <div className="iq-card ">
                                             <div className="iq-card-header d-flex justify-content-between">
                                                 <div className="iq-header-title">
-                                                    <h4 className="card-title">Two Days Live Account</h4>
+                                                    <h4 className="card-title">Three Days Live Account</h4>
                                                 </div>
                                             </div>
                                             <div className="iq-card-body">

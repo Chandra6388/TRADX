b4b5c85fd50dcdc190b5de8b96b4fd3915a55336
Chandra6388 <cppatel6388@gmail.com>
2024-12-30 16:32:00 +0530

    Enhance AllClient.jsx by adding broker and group fetching logic; implement modal for editing client details with improved validation and form handling.

--- Frontend/src/components/subadmin/Client/Allclient.jsx
+++ Frontend/src/components/subadmin/Client/Allclient.jsx
@@ -2,24 +2,39 @@ import React, { useEffect, useState } from 'react';
 import { GetAllSubadminClient } from '../../CommonAPI/SubAdmin';
 import FullDataTable from '../../../ExtraComponent/CommanDataTable';
 import { Link, useNavigate } from 'react-router-dom';
+import { GetGroupNames, EditClientPanle, Get_Broker_Name } from '../../CommonAPI/Admin';
 import { SquarePen } from 'lucide-react';
-
+import AddForm from '../../../ExtraComponent/FormData';
+import { useFormik } from 'formik';
+import Swal from 'sweetalert2';
+import Select from 'react-select';
 
 
 
 const AllClient = () => {
-   const userName = localStorage.getItem('name');
+    const userName = localStorage.getItem('name');
     const navigate = useNavigate();
     const [clientService, setClientService] = useState({ loading: true, data: [] });
     const [searchInput, setSearchInput] = useState('')
+    const [showModal, setShowModal] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState([]);
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [brokers, setBrokers] = useState([]);
+    const [optionsArray, setOptionsArray] = useState([]);
 
 
     useEffect(() => {
         fetchAllSubadmin();
     }, [searchInput]);
 
+    useEffect(() => {
+        fetchBrokerName();
+        fetchGroupDetails();
+    }, []);
+
+
     const fetchAllSubadmin = async () => {
-        const req = {userName: userName}
+        const req = { userName: userName }
         try {
             const response = await GetAllSubadminClient(req);
             if (response.Status) {
@@ -45,12 +60,42 @@ const AllClient = () => {
         }
     };
 
+    const fetchBrokerName = async () => {
+        try {
+            const response = await Get_Broker_Name();
+            if (response.Status) {
+                const brokerList = response.Brokernamelist.filter(item => item.BrokerName !== 'DEMO');
+                setBrokers(brokerList);
+            } else {
+                setBrokers([]);
+            }
+        } catch (error) {
+            console.log('Error in fetching brokers', error);
+        }
+    };
+
+    const fetchGroupDetails = async () => {
+        try {
+            const response = await GetGroupNames();
+            if (response.Status) {
+                const options = response.Data.map(item => ({
+                    label: item.GroupName,
+                    value: item.GroupName,
+                }));
+                setOptionsArray(options);
+
+            } else {
+                setOptionsArray([]);
+            }
+        } catch (error) {
+            console.log('Error in fetching group data', error);
+        }
+    };
+
 
     const EditSubadmindetail = (value, tableMeta) => {
-        const rowData = clientService.data[tableMeta.rowIndex];
-        navigate(`/admin/editSubadmin`, {
-            state: { rowData },
-        });
+        setShowModal(true);
+        setSelectedIndex(clientService.data[tableMeta.rowIndex]);
     };
 
 
@@ -61,7 +106,7 @@ const AllClient = () => {
             label: 'S.No',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value, tableMeta) => tableMeta.rowIndex + 1,
             },
         },
@@ -70,7 +115,7 @@ const AllClient = () => {
             label: 'Edit',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value, tableMeta) => (
                     <SquarePen
                         onClick={() => EditSubadmindetail(value, tableMeta)} />
@@ -83,7 +128,7 @@ const AllClient = () => {
             label: 'Username',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value) => value || '-'
             }
         },
@@ -92,7 +137,7 @@ const AllClient = () => {
             label: 'Email ID',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value) => value || '-'
             }
         },
@@ -101,7 +146,7 @@ const AllClient = () => {
             label: 'Mobile Number',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value) => value || '-'
             }
         },
@@ -110,7 +155,7 @@ const AllClient = () => {
             label: 'Broker Name',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value) => value || '-'
             }
         },
@@ -119,7 +164,7 @@ const AllClient = () => {
             label: 'Licanse',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value) => value || '-'
             }
         },
@@ -128,7 +173,7 @@ const AllClient = () => {
             label: 'Create Date',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value) => value || '-'
             }
         },
@@ -137,7 +182,7 @@ const AllClient = () => {
             label: 'Service Start Date',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value) => value || '-'
             }
         },
@@ -146,7 +191,7 @@ const AllClient = () => {
             label: 'Service End Date',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value) => value || '-'
             }
         },
@@ -155,7 +200,7 @@ const AllClient = () => {
             label: 'Group',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value) => value?.join(', ') || '-'
             }
         },
@@ -164,14 +209,90 @@ const AllClient = () => {
             label: 'Plan Name',
             options: {
                 filter: true,
-                sort: true,
+                sort: false,
                 customBodyRender: (value) => value?.join(', ') || '-'
             }
         },
-        
+
 
     ];
 
+    const formik = useFormik({
+        initialValues: {
+            User: "",
+            Broker: "",
+            GroupName: "",
+        },
+        validate: values => {
+            const errors = {};
+            if (!values.User && showModal) {
+                errors.User = 'Please enter the User';
+            }
+            if (!values.Broker && showModal) {
+                errors.Broker = 'Please Select the Broker';
+            }
+            return errors;
+        },
+
+
+        onSubmit: async (values) => {
+            const req = {
+                User: values.User,
+                GroupName: selectedOptions.map(item => item.value),
+                Broker: values.Broker,
+            }
+            try {
+                const response = await EditClientPanle(req);
+                if (response.Status) {
+                    Swal.fire({
+                        title: "Updated",
+                        text: response.message,
+                        icon: "success",
+                        timer: 1500,
+                        timerProgressBar: true
+                    });
+                    setTimeout(() => {
+                        setShowModal(false);
+                        formik.resetForm();
+                        setSelectedOptions([]);
+                    }, 1500);
+                    fetchAllSubadmin();
+                } else {
+                    Swal.fire({
+                        title: "Error",
+                        text: response.message,
+                        icon: "error",
+                        timer: 1500,
+                        timerProgressBar: true
+                    });
+                }
+            } catch (err) {
+                console.log("Error in update client", err);
+            }
+        },
+    });
+
+
+    const fields = [
+        {
+            name: 'Broker',
+            label: 'Broker',
+            type: 'select',
+            options: brokers && brokers.map(item => ({ label: item.BrokerName, value: item.BrokerName })),
+            label_size: 12,
+            col_size: 12,
+        },
+
+    ];
+
+
+    useEffect(() => {
+        if (showModal) {
+            formik.setFieldValue('Broker', selectedIndex?.BrokerName == 'Demo' ? "" : selectedIndex?.BrokerName);
+            formik.setFieldValue('User', selectedIndex?.Username);
+            setSelectedOptions(showModal && selectedIndex?.Group.map(item => ({ label: item, value: item })));
+        }
+    }, [showModal])
 
 
     return (
@@ -195,6 +316,57 @@ const AllClient = () => {
                         </div>
                     </div>
                 </div>
+                {showModal && (
+                    <div className='modal custom-modal d-flex' id='add_vendor' role='dialog'>
+                        <div className='modal-dialog modal-dialog-centered modal-lg'>
+                            <div className='modal-content'>
+                                <div className='modal-header clientheader border-0 pb-0'>
+                                    <div className='form-header modal-header-title text-start mb-0'>
+                                        <h4 className='mb-0'>Edit Client : {selectedIndex?.Username}</h4>
+                                    </div>
+                                    <button
+                                        type='button'
+                                        className='btn-close'
+                                        data-bs-dismiss='modal'
+                                        aria-label='Close'
+                                        onClick={() => {
+                                            setShowModal(false);
+                                            formik.resetForm();
+                                            setSelectedOptions([]);
+                                        }}
+                                    ></button>
+                                </div>
+                                <hr />
+                                <AddForm
+                                    fields={fields.filter(
+                                        field => !field.showWhen || field.showWhen(formik.values)
+                                    )}
+                                    btn_name='Update'
+                                    formik={formik}
+                                    btn_name1_route='/admin/clientservice'
+                                    additional_field={
+                                        <div className='mt-2'>
+                                            <div className='row'>
+                                                <div className='col-lg-12'>
+                                                    <h6>Select Group</h6>
+                                                    <Select
+                                                        value={selectedOptions}
+                                                        isMulti
+                                                        name="colors"
+                                                        options={optionsArray}
+                                                        className="basic-multi-select"
+                                                        classNamePrefix="select"
+                                                        onChange={(selected) => setSelectedOptions(selected)}
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    }
+                                />
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
 
         </>

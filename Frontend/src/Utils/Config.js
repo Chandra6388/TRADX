dc2b0eb4052cd309240b7eaa58b5bfbaa8cfe7ce
Chandra6388 <cppatel6388@gmail.com>
2025-01-03 12:40:38 +0530

    Uncomment production base_url in Config.js and comment out local backend URL

--- Frontend/src/Utils/Config.js
+++ Frontend/src/Utils/Config.js
@@ -1,5 +1,5 @@
 // export const react_domain = `${window.location.origin}`;
-export const base_url = `${window.location.origin}/backend/`
+// export const base_url = `${window.location.origin}/backend/`
 // export const base_url = `http://193.239.237.144:8002/`;
 // export const base_url = `http://193.239.237.147:8000/`;
 // export const base_url = `http://193.239.237.149:8000/`;
@@ -7,7 +7,7 @@ export const base_url = `${window.location.origin}/backend/`
 // export const base_url = `https://tools.tradestreet.in/backend/`;
 // export const base_url = `https://tools.nextbrand.tech/backend/`;
 // export const base_url = `https://tools.scalpingalgo.in//backend/`;
-// export const base_url = `https://tools.easytrades.in//backend/`;
+export const base_url = `https://tools.easytrades.in//backend/`;
 
 //if superAdmin login then change the url only for it
 // export const superAdmin_base_url = `${window.location.origin}/superbackend/`
